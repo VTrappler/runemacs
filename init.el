fcef6a30ddc122255b88064e358d478381a4996c
Victor Trappler <victor.trappler@gmail.com>
2022-01-03 09:22:14 +0100

    init, emacs config

---
+++ init.el
@@ -0,0 +1,21 @@
+(org-babel-load-file "~/runemacs/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(midnight-mode t)
+ '(org-agenda-files '("/home/victor/runemacs/config.org"))
+ '(package-selected-packages
+   '(org-roam-ui rust-mode docker dockerfile-mode markdown-preview-eww markdown-preview-mode simple-httpd websocket yatemplate evil evil-tutor org-evil powerline powerline-evil auto-complete-auctex all-the-icons-completion all-the-icons-gnus all-the-icons-ibuffer org-roam-bibtex dashboard markdown-mode auto-virtualenv sphinx-doc blacken buffer-move rotate ivy-bibtex py-autopep8 yasnippet-classic-snippets zotxt org-noter org-noter-pdftools org-bullets org-ref auctex magit academic-phrases ox-pandoc ox-reveal org-re-reveal org-roam-server pdf-tools yasnippet-snippets which-key use-package projectile elpy doom-themes doom-modeline counsel all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired))
+ '(warning-suppress-types '(((org-roam)) (use-package) (:warning))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:foreground "#d33682" :weight bold :height 1.5))))
+ '(org-level-1 ((t (:inherit outline-1 :extend nil :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :extend nil :height 1.1))))
+ '(org-level-3 ((t (:inherit outline-3 :extend nil :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :extend nil :height 1.0)))))
